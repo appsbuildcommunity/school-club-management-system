eddd2b1bdca7f9ddb422fe844130794596347a30
abdessalam <abdesslamaitoubanali@gmail.com>
2026-08-16 13:10:39 +0100

    Feat(database): add liquibase plugin and define all entities migrations

--- back_end/src/main/java/com/appsBuild/club_management_system/model/entity/AssistantMemberPrivilege.java
+++ back_end/src/main/java/com/appsBuild/club_management_system/model/entity/AssistantMemberPrivilege.java
@@ -1,16 +1,8 @@
 package com.appsBuild.club_management_system.model.entity;
 
 import com.appsBuild.club_management_system.model.enums.Privilege;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
+
 import java.util.Date;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -18,6 +10,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "assistant_member_privilege")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
--- back_end/src/main/java/com/appsBuild/club_management_system/model/entity/ProfilePicture.java
+++ back_end/src/main/java/com/appsBuild/club_management_system/model/entity/ProfilePicture.java
@@ -1,19 +1,13 @@
 package com.appsBuild.club_management_system.model.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table( name = "profile_picture")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -26,11 +20,9 @@ public class ProfilePicture {
   @Column(name = "s3_key", nullable = false, length = 500)
   private String s3Key;
 
-  @OneToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "user_id", referencedColumnName = "user_id")
+  @OneToOne(mappedBy = "profilePicture")
   private User user;
 
-  @OneToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "club_id", referencedColumnName = "club_id")
+  @OneToOne(mappedBy = "profilePicture")
   private Club club;
 }
