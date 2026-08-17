f35b8bb1f7408e9313cd2a3c5b2feb4301359469
abdessalam <abdesslamaitoubanali@gmail.com>
2026-08-18 00:49:19 +0100

    test(database): add test-container to test database and make integration tests in future

---
+++ back_end/src/test/java/com/appsBuild/club_management_system/db/DatabaseMigrationTest.java
@@ -0,0 +1,36 @@
+package com.appsBuild.club_management_system.db;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.postgresql.PostgreSQLContainer;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class DatabaseMigrationTest {
+    static PostgreSQLContainer postgres = new PostgreSQLContainer("postgres:18");
+
+    @BeforeAll
+    static void beforeAll() {
+        postgres.start();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        postgres.stop();
+    }
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+
+    @Test
+    void contextLoadsAndMigrationsExecuteSuccessfully() {
+
+    }
+}
