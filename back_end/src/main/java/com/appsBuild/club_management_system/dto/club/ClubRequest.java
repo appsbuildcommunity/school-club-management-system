b3279525586c4cee2a9be5fdf5b4b56eb9fd5368
tawfiq akdir <tawfiqakdir@gmail.com>
2026-08-22 15:25:39 +0100

    feat(club): add DTOs for club CRUD endpoints

---
+++ back_end/src/main/java/com/appsBuild/club_management_system/dto/club/ClubRequest.java
@@ -0,0 +1,4 @@
+package com.appsBuild.club_management_system.dto.club;
+
+public record ClubRequest(
+    String clubName, String clubFullName, String description, String presidentUsername) {}
---
+++ back_end/src/main/java/com/appsBuild/club_management_system/dto/club/PresidentRequest.java
@@ -0,0 +1,3 @@
+package com.appsBuild.club_management_system.dto.club;
+
+public record PresidentRequest(String presidentUsername) {}
